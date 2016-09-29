701970572846c37c32427c41ba401445fbe76e74
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-09-29 15:01:53 -0700

    run server so angular can pull data from json file and not get cross-origin error

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require ('express'); 
+var app = express(); 
+var fs = require('fs'); 
+var path = require('path'); 
+
+app.use(express.static(path.join(__dirname, './'))); 
+
+
+app.get('/', function(req, res){
+    res.send('./index.html'); 
+});
+
+
+app.listen(4000);
+console.log("you're up on 4000");  
